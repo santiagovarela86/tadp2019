0b75fac65aab906a85ca190deb34586bf9136ec3
santiagovarela86 <santiagovarela.86@gmail.com>
2019-04-26 13:05:46 -0300

    Corrijo el Antes y Despues, y comienzo con Invariantes

--- ruby/lib/antes_despues.rb
+++ ruby/lib/antes_despues.rb
@@ -1,3 +1,5 @@
+#######################################
+#######################################
 class Class
   def before_and_after_each_call(bef, aft)
     @befs ||= []
@@ -7,11 +9,18 @@ class Class
   end
 
   def method_added(original_method_name)
-    unless @already_replacing_method or original_method_name.equal?(:method_added)
-        @already_replacing_method = true
+    unless @already_replacing_method \
+      or Class.instance_methods.include?(original_method_name)
+        #or self.instance_methods.include?(original_method_name) \
+          #or original_method_name.equal?(:method_added) \
+            #or original_method_name.equal?(:invariant) \
+              #or original_method_name.equal?(:initialize)
+      @already_replacing_method = true
         original_method_object = instance_method(original_method_name)
-        bef_procs = @befs
-        aft_procs = @afts
+        bef_procs = @befs.nil? ? [] : @befs
+        aft_procs = @afts.nil? ? [] : @afts
+        #bef_procs = @befs
+        #aft_procs = @afts
         define_method(original_method_name) do |*args, &block|
           bef_procs.each {|x| self.instance_eval(&x)}
           result = original_method_object.bind(self).call(*args, &block)
@@ -22,7 +31,16 @@ class Class
     end
   end
 end
-
+#######################################
+class Class
+  def invariant(&block)
+    #self.instance_eval(&block)
+    #unless block.call
+    #  puts "exception"
+    #end
+  end
+end
+#######################################
 class MyClass
   before_and_after_each_call(proc{ puts "Entré a un mensaje1" },
                              proc{ puts "Salí de un mensaje1" })
@@ -50,7 +68,7 @@ class MyClass
     10
   end
 end
-
+#######################################
 class AnotherClass
   before_and_after_each_call(proc{ puts "Into another message1" },
                              proc{ puts "Out of another message1" })
@@ -61,7 +79,20 @@ class AnotherClass
     puts "Another message"
   end
 end
+#######################################
+class TestInvariant
+  attr_accessor :vida, :fuerza
 
+  def initialize(vida=100, fuerza=100)
+    @vida = vida
+    @fuerza = fuerza
+  end
+
+  invariant { vida >= 0 }
+  #invariant { fuerza > 0 && fuerza < 100 }
+end
+#######################################
+#######################################
 obj = MyClass.new
 obj.hola("un parametro")
 obj.chau
@@ -74,3 +105,6 @@ puts a
 
 obj3 = AnotherClass.new
 obj3.another_method
+#######################################
+#######################################
+obj5 = TestInvariant.new(100,100)
