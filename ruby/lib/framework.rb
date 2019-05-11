4b93b08964bbae229a61d2319fc6fa91d13c419b
lucasmessinar <lucasmessinar@gmail.com>
2019-05-11 00:01:12 -0300

    feat: exclusions

--- ruby/lib/framework.rb
+++ ruby/lib/framework.rb
@@ -1,6 +1,6 @@
 module BeforeAndAfter
 
-  def beforeAndAfter(observed)
+  def before_and_after(observed)
 
     observed.instance_variable_set :@before_procs, []
     observed.instance_variable_set :@after_procs, []
@@ -28,7 +28,7 @@ end
 
 module InvariantProcs
 
-  def invariantProcs(observed)
+  def invariant_procs(observed)
     observed.instance_variable_set :@invariant_procs, []
 
     def observed.invariant(&invariant_proc)
@@ -39,23 +39,75 @@ module InvariantProcs
 
 end
 
+module PreAndPost
+  def pre_and_post(observed)
+
+    observed.instance_variable_set :@pre_proc, nil
+    observed.instance_variable_set :@post_roc, nil
+
+
+    def observed.pre(&preCondition)
+      @pre_proc = preCondition
+    end
+
+    def observed.post(&preCondition)
+      @pre_proc = preCondition
+    end
+
+    def observed.pre
+      pre_tu_return = @pre_proc
+      @pre_proc = nil
+      pre_tu_return
+    end
+
+    def observed.post
+      post_tu_return = @post_proc
+      @post_proc = nil
+      post_tu_return
+    end
+
+  end
+end
+
+module Exclusions
+
+  def method_exclusions(observed)
+
+    observed.instance_variable_set :@exclusions, ['initialize']
+
+    def observed.exclude(method_name)
+      @exclusions = method_name
+    end
+
+    def observed.is_excluded?(method_name)
+      @exclusions.include?method_name
+    end
+
+  end
+
+end
+
 
 module MyMixin
   extend BeforeAndAfter
   extend InvariantProcs
+  extend PreAndPost
+  extend Exclusions
 
   def self.included(observed)
 
-    beforeAndAfter(observed)
-    invariantProcs(observed)
+    before_and_after(observed)
+    invariant_procs(observed)
+    pre_and_post(observed)
+    method_exclusions(observed)
 
-    observed.instance_methods(false).each do |func|
+    observed.instance_methods(false).select{|method_name| !is_excluded?(method_name) }.each do |func|
       inject(observed, func)
     end
 
     #Override the singletons method_added to ensure all future methods are injected.
     def observed.method_added(meth)
-      unless @called_internal
+      unless @called_internal || is_excluded?(meth)
         @called_internal = true
         MyMixin.inject(self, meth) #This will call method_added itself, the condition prevents infinite recursion.
         @called_internal = false
@@ -72,7 +124,6 @@ module MyMixin
         procs_to_call_before = self.class.before_procs
         procs_to_call_after = self.class.after_procs
         procs_to_call_after_invariants = self.class.invariant_procs
-
         procs_to_call_before.each {|x| self.instance_eval(&x)}
         result = method_object.bind(self).call(*args, &block)
         procs_to_call_after.each {|x| self.instance_eval(&x)}
