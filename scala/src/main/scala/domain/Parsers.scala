666e5ec31e48131b79ad6e03970bd5042643f0f2
santiagovarela86 <santiagovarela.86@gmail.com>
2019-06-22 20:26:02 -0300

    Corren los tests de anychar y char

--- scala/src/main/scala/domain/Parsers.scala
+++ scala/src/main/scala/domain/Parsers.scala
@@ -2,39 +2,51 @@ package domain
 
 import scala.util.{Failure, Success, Try}
 
-class Parseo(val string: Seq[Char]) {
-  def getCaracter(): Char = string.head
-  def getString(): Seq[Char] = string.tail
+class Parseo(val string: String) {
+  def getValor(): Char = string.head
+  def getResto(): String = string.tail
 }
 
 trait Parser
 
+//VER COMO SACAR LAS VERIFICACIONES POR EMPTY EN TODOS LOS PARSERS
+//ASI COMO ESTA FUNCIONAN LOS TESTS SEGUN LAS ESPECIFICACIONES
+//(debe dar failure el string de entrada vacio en todos los parsers)
 case object anyChar extends Parser {
-  def apply(string: Seq[Char]): Try[Parseo] = Try(new Parseo(string))
+  def apply(string: String): Try[Parseo] = 
+    {
+      if (string.isEmpty()) Failure(new Throwable) else
+        Success(new Parseo(string))
+    }
 }
 
 case object char extends Parser {
-  def apply(string: Seq[Char], caracter: Char): Try[Parseo] = if (string.head == caracter) Success(new Parseo(string)) else Failure(new Throwable)
+  def apply(string: String, caracter: Char): Try[Parseo] = 
+    {
+      if (string.isEmpty()) Failure(new Throwable) else
+        if (string.head == caracter) Success(new Parseo(string)) else 
+          Failure(new Throwable)
+    }
 }
 
 case object void extends Parser {
-  def apply(string: Seq[Char]): Try[Parseo] = Try(new Parseo(string.tail))
+  def apply(string: String): Try[Parseo] = Try(new Parseo(string.tail))
 }
 
 case object letter extends Parser {
-  def apply(string: Seq[Char]): Try[Parseo] = if (string.head.isLetter) Success(new Parseo(string)) else Failure(new Throwable)
+  def apply(string: String): Try[Parseo] = if (string.head.isLetter) Success(new Parseo(string)) else Failure(new Throwable)
 }
 
 case object digit extends Parser {
-  def apply(string: Seq[Char]): Try[Parseo] = if (string.head.isDigit) Success(new Parseo(string)) else Failure(new Throwable)
+  def apply(string: String): Try[Parseo] = if (string.head.isDigit) Success(new Parseo(string)) else Failure(new Throwable)
 }
 
 case object alphaNum extends Parser {
-  def apply(string: Seq[Char]): Try[Parseo] = if (string.head.isLetter || string.head.isDigit) Success(new Parseo(string)) else Failure(new Throwable)
+  def apply(string: String): Try[Parseo] = if (string.head.isLetter || string.head.isDigit) Success(new Parseo(string)) else Failure(new Throwable)
 }
 
 case object string extends Parser {
-  def apply(string: String,substring: String): Try[Parseo] = if (string.startsWith(substring)) Success(new Parseo(string)) else Failure(new Throwable)
+  def apply(string: String, substring: String): Try[Parseo] = if (string.startsWith(substring)) Success(new Parseo(string)) else Failure(new Throwable)
 }
 
 
@@ -42,7 +54,7 @@ case object string extends Parser {
 // Imprimiendo valores
 object Algo {
   def show(x: Try[Parseo]) = x match {
-    case Success(s) => print(s.getCaracter())
+    case Success(s) => print(s.getValor())
     case Failure(a) => print(a.getCause)
   }
 }
--- scala/src/test/scala/domain/ProjectSpec.scala
+++ scala/src/test/scala/domain/ProjectSpec.scala
@@ -4,46 +4,46 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.Assert._
 
-class anyChar_Parser_Test {  
+class Parser_anyChar_Test {  
   @Before
   def setup() = {
   }
   
   @Test
   def anychar_test_successful() = {
-    var resultado = anyChar.apply("hola".toList)
+    var resultado = anyChar ("hola")
     assertEquals(true, resultado.isSuccess)
-    assertEquals('h', resultado.get.getCaracter())
+    assertEquals('h', resultado.get.getValor())
   }
   
   @Test
   def anychar_test_failure() = {
-    var resultado = anyChar.apply("".toList)
-    assertEquals(false, resultado.isSuccess) //ESTO FALLA
+    var resultado = anyChar ("")
+    assertEquals(false, resultado.isSuccess)
   }
 }
 
-class char_Parser_Test {  
+class Parser_char_Test {  
   @Before
   def setup() = {
   }
   
   @Test
   def char_test_successful() = {
-    var resultado = char.apply("hola", 'h')
+    var resultado = char ("hola", 'h')
     assertEquals(true, resultado.isSuccess)
-    assertEquals('h', resultado.get.getCaracter())
+    assertEquals('h', resultado.get.getValor())
   }
   
   @Test
   def char_test_failure_char() = {
-    var resultado = char.apply("hola", 'c')
+    var resultado = char ("hola", 'c')
     assertEquals(false, resultado.isSuccess)
   }
   
   @Test
   def char_test_failure_empty() = {
-    var resultado = char.apply("", 'c') //NOSUCH ELEMENTEXCEPTION
+    var resultado = char ("", 'c')
     assertEquals(false, resultado.isSuccess)
   }
 }
